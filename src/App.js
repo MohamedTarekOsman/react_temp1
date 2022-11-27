a713b4d802aa61672d63a6c56964d62aa2397fd7
mohamed <mt469063@gmail.com>
2022-11-27 15:26:25 +0200

    version 1.0

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
 import './App.css';
-
+import Mynav from './Mynav';
+import "bootstrap/dist/css/bootstrap.css"
+import Slider from './Slider';
+import Footer from './Footer';
+import Products from './Products';
+import Counter from './Counter';
+import Error from './Error';
+import { Route, Routes } from 'react-router-dom';
+import Productes_Content from './Productes_Content';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-dark">
+      <Mynav />
+      <Routes>
+        <Route path="/home" element={<Slider />} />
+        <Route path="/productes" element={<Products />} />
+        <Route path="/productes/:id" element={<Productes_Content />} />
+        <Route path="/about" element={<Counter />} />
+        <Route path="*" element={<Error/>} />
+        {/* 
+        
+        <Counter /> */}
+      </Routes>
+      <Footer />
     </div>
   );
 }
